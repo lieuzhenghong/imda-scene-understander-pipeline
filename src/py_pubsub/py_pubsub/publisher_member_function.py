330706902b9cc63713559285ea4b6a69039bc010
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-09-28 17:52:32 +0800

    Add new files

--- src/py_pubsub/py_pubsub/publisher_member_function.py
+++ src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -1,7 +1,16 @@
-'''
-Modified from the following link
-# https://index.ros.org/doc/ros2/Tutorials/Writing-A-Simple-Py-Publisher-And-Subscriber/
-'''
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import rclpy
 from rclpy.node import Node
@@ -13,7 +22,6 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        # self.publisher_ = self.create_publisher(String, 'topic', 10)
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
