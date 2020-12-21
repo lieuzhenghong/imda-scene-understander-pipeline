055c3468c692824fe981373593c7dbc5dc6a29f0
Lieu Zheng Hong <lieu@lieuzhenghong.com>
2020-12-21 12:16:38 +0800

    Add tests to mock socket

--- su-rmf-pipe/src/test_io_handler.py
+++ su-rmf-pipe/src/test_io_handler.py
@@ -1,8 +1,18 @@
 import pyrealsense2 as rs
-import send_cam_stream
 from unittest.mock import MagicMock, create_autospec
 import numpy as np
 import pytest
+import socket
+from io import BytesIO
+
+from typing import List
+import send_cam_stream
+
+####################################################
+# Mock classes
+# Implement mock interfaces for costly objects
+# (sockets, RealSense camera, etc.)
+###################################################
 
 class MockRealSenseConfig:
     def enable_stream(self, *args):
@@ -31,8 +41,50 @@ class MockRealSenseColorFrame:
     def get_data(self):
         return np.random.rand(640, 480, 3)
 
+class MockSocket:
+    # We need enter and exit methods so that it works with a with block
+    def __init__(self):
+        self.all_data_sent = False 
+    def __enter__(self):
+        return self
+    def connect(self, addr):
+        # should return another socket
+        return MockSocket()
+    def sendall(self, all_bytes):
+        # print(all_bytes)
+        print("\nAll bytes sent")
+        return
+    def recv(self, nbytes: int) -> List[bytes]:
+        bboxes = np.random.rand(1, 6)
+        bbox_bytes = BytesIO()
+        np.save(bbox_bytes, bboxes)
+        bbox_bytes.seek(0)
+        if self.all_data_sent:
+            return None
+        else:
+            self.all_data_sent = True
+            return bbox_bytes.read()
+    def close(self):
+        print("\nSocket connection closed")
+        return
+    def __exit__(self, *args, **kwargs):
+        pass
+
+class MockBoundingBox:
+    def __init__(self, x1, y1, x2, y2, s, c):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.s = s
+        self.c = c
+
+# 
+# Monkeypatch library calls to use our mocks 
+# 
+
 @pytest.fixture(autouse=True)
-def mock_init(monkeypatch):
+def mock_rs(monkeypatch):
     def mock_pipeline(*args, **kwargs):
         return MockRealSensePipeline()
 
@@ -42,7 +94,18 @@ def mock_init(monkeypatch):
     monkeypatch.setattr(rs, "pipeline", mock_pipeline)
     monkeypatch.setattr(rs, "config", mock_config)
 
-def test_inite_pipeline():
+@pytest.fixture(autouse=True)
+def mock_socket(monkeypatch):
+    def init_mock_socket(*args, **kwargs):
+        return MockSocket()
+    
+    monkeypatch.setattr(socket, "socket", init_mock_socket)
+
+###############################################################
+# TESTS
+###############################################################
+
+def test_init_pipeline():
     pipeline = send_cam_stream.init_pipeline()
     assert True
 
@@ -58,12 +121,14 @@ def test_get_frames():
 
 # TODO mock the server, make sure to test different combinations of outputs
 def test_send_image_to_server():
-    bboxes = send_cam_stream_to_server()
+    img = np.random.rand(640, 480, 3)
+    bboxes = send_cam_stream.send_image_to_server(img)
+    assert True
 
-def test_loop():
-    loop()
+'''
+def test_canDecodeSuccessfullyNoBBox():
     assert False
 
-def test_main():
-    send_cam_stream.main()
-    assert True
+def test_canDecodeSuccessfullyOneBBox():
+    assert False
+'''
