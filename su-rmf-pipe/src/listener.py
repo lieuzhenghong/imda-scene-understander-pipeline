92dce6a146df0608062f277567d11bdf803a153b
Lieu Zheng Hong <lieu@lieuzhenghong.com>
2020-12-22 08:21:29 +0800

    Refactor listener.py to remove depency on connection

--- su-rmf-pipe/src/listener.py
+++ su-rmf-pipe/src/listener.py
@@ -9,26 +9,41 @@ class Listener:
                 ):
         self.server_addr = server_addr
         self.handle_data_fn = handle_data_fn
-    @staticmethod
-    def recv_all(msg_len, connection):
+        self.connection = None
+    def recv_all(self, msg_len):
         '''
         Receives data of msg_len in chunks of 4096 bits
         and returns a BytesIO filled with that data
         '''
         full_data = BytesIO()
         while (full_data.getbuffer().nbytes < msg_len):
-            data = connection.recv(4096)
+            data = self.connection.recv(4096)
             full_data.write(data)
         full_data.seek(0)
         return full_data
-    def decode_message(self, connection) -> BytesIO:
-        b_msg_len = connection.recv(Listener.HEADER_SIZE)
+    def recv_header(self) -> int:
+        '''
+        Given an active connection,
+        reads the first HEADER_SIZE bytes
+        and returns the length of the rest of the payload
+        '''
+        b_msg_len = self.connection.recv(Listener.HEADER_SIZE)
         msg_len = int(b_msg_len.decode("utf-8"))
         print(msg_len)
+        return msg_len
+    def handle_connection(self):
+        '''
+        Once a connection is received,
+        we receive the full data in bytes,
+        perform some function on that data,
+        and return some response to the client.
+        '''
+        msg_len = self.recv_header()
         # receive the data in small chunks
-        full_data = Listener.recv_all(msg_len, connection)
+        b_full_data = self.recv_all(msg_len)
         print("All data received!")
-        return full_data
+        b_return_data = self.handle_data_fn(b_full_data)
+        self.connection.sendall(b_return_data.read())
     def start_server(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -37,18 +52,12 @@ class Listener:
             try:
                 while True:
                     print('waiting for a connection')
-                    connection, client_address = sock.accept()
+                    self.connection, client_address = sock.accept()
+                    print(f"Connection from: ", client_address)
                     try:
-                        print(f"Connection from: ", client_address)
-
-                        b_full_data = self.decode_message(connection)
-                        b_return_data = self.handle_data_fn(b_full_data)
-
-                        connection.sendall(b_return_data.read())
-
-                    # Use a try:finally block to close even in the event of an error
+                        self.handle_connection()
                     finally:
-                        connection.close()
+                        self.connection.close()
             finally:
                 print("Closing socket..")
             sock.close()
