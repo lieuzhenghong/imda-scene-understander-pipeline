90579c73afca4026f0291b5f454cf733302506fe
Lieu Zheng Hong <lieu@lieuzhenghong.com>
2021-01-05 11:27:56 +0800

    Lint all files with Black

--- su-rmf-pipe/src/listener.py
+++ su-rmf-pipe/src/listener.py
@@ -1,67 +1,73 @@
 from io import BytesIO
 import socket
 
+
 class Listener:
     from typing import Tuple
+
     HEADER_SIZE = 10
-    def __init__(self, server_addr: Tuple[str, int], 
-                handle_data_fn
-                ):
+
+    def __init__(self, server_addr: Tuple[str, int], handle_data_fn):
         self.server_addr = server_addr
         self.handle_data_fn = handle_data_fn
         self.connection = None
+
     def recv_all(self, msg_len):
-        '''
+        """
         Receives data of msg_len in chunks of 4096 bits
         and returns a BytesIO filled with that data
-        '''
+        """
         full_data = BytesIO()
-        while (full_data.getbuffer().nbytes < msg_len):
+        while full_data.getbuffer().nbytes < msg_len:
             data = self.connection.recv(4096)
             full_data.write(data)
         full_data.seek(0)
         return full_data
+
     def recv_header(self) -> int:
-        '''
+        """
         Given an active connection,
         reads the first HEADER_SIZE bytes
         and returns the length of the rest of the payload
-        '''
+        """
         b_msg_len = self.connection.recv(Listener.HEADER_SIZE)
         msg_len = int(b_msg_len.decode("utf-8"))
         print(msg_len)
         return msg_len
+
     def handle_connection(self):
-        '''
+        """
         Once a connection is received,
         we receive the full data in bytes,
         perform some function on that data,
         and return some response to the client.
-        '''
+        """
         msg_len = self.recv_header()
         # receive the data in small chunks
         b_full_data = self.recv_all(msg_len)
         print("All data received!")
         b_return_data = self.handle_data_fn(b_full_data)
         self.connection.sendall(b_return_data.read())
+
     def init_socket(self):
-        '''
+        """
         Creates a socket,
         binds it to the server addr
         and listens for connections
-        '''
+        """
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind(self.server_addr)
         sock.listen(1)
         self.sock = sock
         return sock
+
     def wait_for_connection(self):
-        '''
+        """
         Waits for any incoming connection
         When one is received,
         handles it and then closes the connection
-        '''
+        """
         # self.connection is a new socket object
         # and client_address is the address bound to the socket
         # on the other hand of the connection
@@ -71,6 +77,7 @@ class Listener:
             self.handle_connection()
         finally:
             self.connection.close()
+
     def start_server(self):
         try:
             self.init_socket()
--- su-rmf-pipe/src/mlbbox_listener.py
+++ su-rmf-pipe/src/mlbbox_listener.py
@@ -1,18 +1,20 @@
-'''
+"""
 === TCP server code ===
 Sets up a listener on port 6000 and waits for a datastream.
 Receives the data from the client (a serialised numpy array),
 passes it to James's `detect` function in B1_detect.py
 and returns the results.
-'''
+"""
 
 import sys
 from io import BytesIO
 import numpy as np
 from listener import Listener
 
+
 def call_detect_function_and_recv_result(b_full_data) -> BytesIO:
     from B1_detect import detect
+
     # Now sending the data to the ML model
     print("Sending data to ML model...")
     b_full_data.seek(0)
